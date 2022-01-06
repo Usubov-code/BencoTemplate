d3c3c31eea35a52ee90338295213b541df3c4bdf
Elshad Usubov <92145698+Usubov-code@users.noreply.github.com>
2022-01-06 22:41:12 +0400

    Faq Table Crud

---
+++ BencoTemplate/Areas/admin/Controllers/FaqController.cs
@@ -0,0 +1,47 @@
+﻿using BencoTemplate.Data;
+using BencoTemplate.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BencoTemplate.Areas.admin.Controllers
+{
+    [Area("admin")]
+    public class FaqController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public FaqController(AppDbContext context)
+        {
+           _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View(_context.Faqs.ToList());
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Faq model )
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Faqs.Add(model);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+           
+
+
+            return View(model);
+        }
+         
+
+    }
+}
--- BencoTemplate/Startup.cs
+++ BencoTemplate/Startup.cs
@@ -51,6 +51,11 @@ namespace BencoTemplate
 
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapAreaControllerRoute(
+                     name: "Areas",
+                     areaName: "admin",
+                     pattern: "admin/{controller=Account}/{action=Login}/{id?}");
+
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");

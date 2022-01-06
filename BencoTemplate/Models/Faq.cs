ef03663b6aa6c27b2f8625b0df2194c5d44bb705
Elshad Usubov <92145698+Usubov-code@users.noreply.github.com>
2022-01-06 12:31:34 +0400

    Son Faq

---
+++ BencoTemplate/Models/Faq.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BencoTemplate.Models
+{
+    public class Faq
+    {
+
+        [Key]
+        public int Id { get; set; }
+        [MaxLength(50)]
+        public string Title { get; set; }
+        [MaxLength(250)]
+        public string Content { get; set; }
+        [MaxLength(150)]
+        public string CollapseText { get; set; }
+        [MaxLength(500)]
+        public string CollapseContent { get; set; }
+    }
+}

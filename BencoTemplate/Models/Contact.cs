d3dd3ffb7f3b69216a1a8f04be27a0bf7a975d01
Elshad Usubov <92145698+Usubov-code@users.noreply.github.com>
2022-01-06 12:26:44 +0400

    Add project files.

---
+++ BencoTemplate/Models/Contact.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BencoTemplate.Models
+{
+    public class Contact
+    {
+        [Key]
+        public int Id { get; set; }
+        [MaxLength(250)]
+        public string MapsLink { get; set; }
+        [MaxLength(50)]
+        public string Email { get; set; }
+        [MaxLength(150)]
+        public string Name { get; set; }
+        [MaxLength(100)]
+        public string LastName { get; set; }
+        [MaxLength(250)]
+        public string Subject { get; set; }
+        [MaxLength(250)]
+        public string Message { get; set; }
+
+        public DateTime CreatedTime { get; set; }
+    }
+}
---
+++ BencoTemplate/Models/Setting.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BencoTemplate.Models
+{
+    public class Setting
+    {
+        [Key]
+        public int Id { get; set; }
+        [MaxLength(250)]
+        public string Phone { get; set; }
+        [MaxLength(50)]
+        public string Email { get; set; }
+        [MaxLength(150)]
+        public string Adress { get; set; }
+        [MaxLength(100)]
+        public string WorkTime { get; set; }
+        [MaxLength(250)]
+        public string Logo { get; set; }
+    }
+}
